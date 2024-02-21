e02062fe28d1b96092d414c99aff5618a09d9ab5
valentino-dev <angelo.valentino.brade@gmail.com>
2024-02-21 16:31:39 +0100

    tut3

---
+++ C_Kurs/tut3/src/omod.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef omod
+#define omod
+
+extern const double EPSILON;
+
+int sgn();
+double betrag();
+double naiv_pow_own();
+double pow_own();
+double pow_own_loop();
+long long fac();
+double expo();
+double logarithm();
+double power();
+double zeta_function();
+double cos();
+double wurzel();
+
+#endif
